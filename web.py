5f1ac6d7501a996fbe74b510ec62de7c5b592cee
jasonhernacki <50277306+jasonhernacki@users.noreply.github.com>
2019-05-04 20:25:25 -0400

    Update web.py

--- web.py
+++ web.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-  return 'Hello World'
+@app.route("/")
+def hello():
+    return render_template('tf_idf_tree.html')
